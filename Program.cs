5a0cb1d2a4408a24a1ace66b92c86f416fba8ca1
barmc <151999059+barmcoovy@users.noreply.github.com>
2024-04-24 11:29:05 +0200

    project part 1

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace osoba_konsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nazwisko;
+            double wzrost, masa, obwodPasa, prawidlowyObwodPassa;
+            char plec;
+            Console.WriteLine("Podaj nazwisko:");
+            nazwisko = Console.ReadLine();
+            Console.WriteLine("Podaj wzrost:");
+            wzrost = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj masę:");
+            masa = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj obwodPasa:");
+            obwodPasa = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj płeć (M/K):");
+            plec = Convert.ToChar(Console.ReadLine());
+            Osoba osoba1 = new Osoba();
+            osoba1.Nazwisko = nazwisko;
+            osoba1.Wzrost = wzrost;
+            osoba1.Masa = masa;
+            osoba1.ObwodPasa = obwodPasa;
+            osoba1.Plec = plec;
+
+            prawidlowyObwodPassa =  osoba1.obliczPrawidlowyObwodPasa(wzrost, plec);
+            if(obwodPasa == prawidlowyObwodPassa || obwodPasa>prawidlowyObwodPassa-10|| obwodPasa < prawidlowyObwodPassa + 10)
+            {
+                Console.WriteLine("Twój obwód pasa jest prawidłowy");
+            }
+
+            Console.ReadKey();
+        }
+
+        public class Osoba : AbstrajcyjnaKlasa
+        {
+            public string Nazwisko {  get; set; }
+            public double Wzrost { get; set; }
+            public double Masa { get; set; }
+            public double ObwodPasa { get; set; }
+            public char Plec { get; set; }
+        }
+        public class AbstrajcyjnaKlasa
+        {
+            public virtual double obliczPrawidlowyObwodPasa(double wzrost, char plec)
+            {
+                double PrawidlowyObwodPasa = 0;
+                if(plec == 'M')
+                {
+                    PrawidlowyObwodPasa = (wzrost / 2);
+                }
+                if(plec == 'K')
+                {
+                    PrawidlowyObwodPasa = (wzrost / 2.2);
+                }
+                return PrawidlowyObwodPasa;
+            }
+        }
+    }
+}
